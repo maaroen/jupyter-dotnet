ca73a7ce450c42bec4c8e2b98758dff61e6e033b
Jeroen Nederlof <jeroennederlof1@msn.com>
2020-11-27 13:57:06 +0100

    changed storage location to save config + workbooks in azure

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ ! -z "${JUPYTER_PASSWORD_HASH}" ]]; then
-  jupyter lab --NotebookApp.token='' --NotebookApp.password='${JUPYTER_PASSWORD_HASH}'
+  jupyter lab --NotebookApp.token='' --NotebookApp.password='${JUPYTER_PASSWORD_HASH}' --notebook-dir='~/notebook/work' --config='~/notebook/.jupyter/jupyter_notebook_config.py'
 elif [[ "${JUPYTER_NO_PASSWORD}" == "true" ]]; then
-  jupyter lab --NotebookApp.token='' --NotebookApp.password=''
+  jupyter lab --NotebookApp.token='' --NotebookApp.password='' --notebook-dir='~/notebook/work' --config='~/notebook/.jupyter/jupyter_notebook_config.py'
 else
-  jupyter lab --NotebookApp.token=''
+  jupyter lab --NotebookApp.token='' --notebook-dir='~/notebook/work' --config='~/notebook/.jupyter/jupyter_notebook_config.py'
 fi
